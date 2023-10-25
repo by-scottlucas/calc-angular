53ad7494570d7195584b279865eb0a9b0ab6c577
Lucas S <Lucasluke307@gmail.com>
2023-10-25 15:37:36 -0300

    Updates

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -8,52 +8,57 @@ import { Component, Input } from '@angular/core';
 
 export class TemplateComponent {
 
+  n1: number = 1;
+  n2: number = 2;
+  n3: number = 3;
+  n4: number = 4;
+  n5: number = 5;
+  n6: number = 6;
+  n7: number = 7;
+  n8: number = 8;
+  n9: number = 9;
+  n0: number = 0;
 
-  zero : number = 0;
+  display : string = '';
 
-  n1 : number = 1;
-  n2 : number = 0;
-  n3 : number = 0;
-  n4 : number = 0;
-  n5 : number = 0;
-  n6 : number = 0;
-  n7 : number = 0;
-  n8 : number = 0;
-  n9 : number = 0;
-  n0 : number = 0;
-
-  num0(){
+  num0() {
     this.n0 = 0;
   }
-  num1(){
-    this.n0 = 1;
+  num1() {
+
+    this.n0 = this.n1;
+
+    if (this.n1) {
+      let numberAsString : string = `${this.n1}`;
+      this.display += numberAsString;
+    }
   }
-  num2(){
-    this.n2 = this.n0 = 2;
+  num2() {
+    this.n2 = 2;
   }
-  num3(){
+  num3() {
     this.n3 = this.n0 = 3;
   }
-  num4(){
+  num4() {
     this.n4 = this.n0 = 4;
   }
-  num5(){
+  num5() {
     this.n5 = this.n0 = 5;
   }
-  num6(){
+  num6() {
     this.n6 = this.n0 = 6;
   }
-  num7(){
+  num7() {
     this.n7 = this.n0 = 7;
   }
-  num8(){
+  num8() {
     this.n8 = this.n0 = 8;
   }
-  num9(){
+  num9() {
     this.n9 = this.n0 = 9;
   }
 
-  constructor() {}
+  constructor() { }
 
 
 }
